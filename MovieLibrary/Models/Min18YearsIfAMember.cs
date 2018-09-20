85738fa9694825cde44e65d03cd80ff472a3e72f
Sashi Datla <sashidatla@gmail.com>
2018-09-20 02:49:11 -0400

    Added Custom Validation for Customer

--- MovieLibrary/Models/Min18YearsIfAMember.cs
+++ MovieLibrary/Models/Min18YearsIfAMember.cs
@@ -5,22 +5,22 @@ namespace MovieLibrary.Models
 {
     public class Min18YearsIfAMember : ValidationAttribute
     {
-    //    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-    //    {
-    //        var customer = (Customer)validationContext.ObjectInstance;
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
 
-    //        if (customer.MembershipTypeId == MembershipType.Unknown ||
-    //            customer.MembershipTypeId == MembershipType.PayAsYouGo)
-    //            return ValidationResult.Success;
+            if (customer.MembershipTypeId == MembershipType.Unknown ||
+                customer.MembershipTypeId == MembershipType.PayAsYouGo)
+                return ValidationResult.Success;
 
-    //        if (customer.Birthdate == null)
-    //            return new ValidationResult("Birthdate is required.");
+            if (customer.Birthdate == null)
+                return new ValidationResult("Birthdate is required.");
 
-    //        var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
+            var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
 
-    //        return (age >= 18)
-    //            ? ValidationResult.Success
-    //            : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
-    //    }
+            return (age >= 18)
+                ? ValidationResult.Success
+                : new ValidationResult("Customer should be at least 18 years old to go on a membership.");
+        }
     }
 }
